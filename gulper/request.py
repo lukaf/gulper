c7e4b33613d51f3afb6c03616a8d768e63530d44
Luka <luka@evriala.net>
2014-06-09 22:16:37 +0100

    Handle HTTP errors.

--- gulper/request.py
+++ gulper/request.py
@@ -12,14 +12,23 @@ class Request(object):
         self.client_id = client_id
         self.api_key = api_key
         self.path = path
+        self.params = {'api_key': self.api_key, 'client_id': self.client_id}
+        if kwargs:
+            self.params.update(kwargs)
         self.rq = '{uri}/{path}?{query}'.format(
             uri=self.uri, path=path,
-            query=urllib.urlencode({'api_key': self.api_key, 'client_id': self.client_id}),
-            **kwargs)
+            query=urllib.urlencode(self.params)
+        )
         log.debug("Constructed URL: %s", self.rq)
 
     def send(self):
-        response = urllib2.urlopen(self.rq).read()
+        try:
+            response = urllib2.urlopen(self.rq).read()
+        except urllib2.HTTPError as err:
+            response = json.loads(err.read())
+            log.error(response['message'])
+            return None
+
         response = json.loads(response)
         if response['status'] == 'OK':
             return response
